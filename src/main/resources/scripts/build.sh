61bdf8e696a5bea095216ded189bdffff41eac41
Georg Sirnes Lundesgaard <glu@capraconsulting.no>
2015-04-09 01:48:27 +0200

    CI prototype

--- src/main/resources/scripts/build.sh
+++ src/main/resources/scripts/build.sh
@@ -6,9 +6,11 @@ cd %s
 git pull
 commit_id=`git rev-parse --short HEAD`
 if [ "$commit_id" != "%s" ]; then
+  echo "Changes detected. Executing build..."
   %s
   cd ..
   echo "last-commit-id=$commit_id" > %s
+  echo "Build completed."
 else
   echo "No changes. Nothing to do."
 fi
