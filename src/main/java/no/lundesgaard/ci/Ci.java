60fc2f5d3ac88212f5281b8b2c22adafbba1e391
Georg Sirnes Lundesgaard <glu@capraconsulting.no>
2015-05-01 21:42:52 +0200

    CI prototype

--- src/main/java/no/lundesgaard/ci/Ci.java
+++ src/main/java/no/lundesgaard/ci/Ci.java
@@ -3,11 +3,11 @@ package no.lundesgaard.ci;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import no.lundesgaard.ci.model.event.EventQueue;
 import no.lundesgaard.ci.model.Type;
 import no.lundesgaard.ci.model.data.Data;
 import no.lundesgaard.ci.model.data.hazelcast.HazelcastData;
 import no.lundesgaard.ci.model.data.simple.SimpleData;
+import no.lundesgaard.ci.model.event.EventQueue;
 import no.lundesgaard.ci.model.job.JobQueue;
 import no.lundesgaard.ci.model.job.Jobs;
 import no.lundesgaard.ci.model.repository.Repositories;
@@ -15,8 +15,8 @@ import no.lundesgaard.ci.model.task.TaskId;
 import no.lundesgaard.ci.model.task.Tasks;
 import no.lundesgaard.ci.processor.CommandProcessor;
 import no.lundesgaard.ci.processor.EventProcessor;
-import no.lundesgaard.ci.processor.JobProcessor;
 import no.lundesgaard.ci.processor.RepositoryProcessor;
+import no.lundesgaard.ci.subscriber.JobSubscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,229 +32,235 @@ import static no.lundesgaard.ci.model.Type.HAZELCAST;
 import static org.apache.commons.lang3.StringUtils.uncapitalize;
 
 public class Ci {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Ci.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(Ci.class);
 
-    public final Path rootPath;
-    public final Path repositoriesPath;
-    public final Path commandsPath;
-    public final Path workspacesPath;
-    private final Type type;
-    private String nodeId;
-    private Data data;
-    public final EventQueue eventQueue = new EventQueue();
-    private CommandProcessor commandProcessor;
-    private JobProcessor jobProcessor;
-    private RepositoryProcessor repositoryProcessor;
-    private EventProcessor eventProcessor;
+	public final Path rootPath;
+	public final Path repositoriesPath;
+	public final Path commandsPath;
+	public final Path workspacesPath;
+	private final Type type;
+	private String nodeId;
+	private Data data;
+	public final EventQueue eventQueue = new EventQueue();
+	private CommandProcessor commandProcessor;
+	private JobSubscriber jobSubscriber;
+	private RepositoryProcessor repositoryProcessor;
+	private EventProcessor eventProcessor;
 
-    public static void main(String[] args) {
-        CiOptions ciOptions = new CiOptions(args);
-        if (ciOptions.isValid()) {
-            new Ci(ciOptions).start();
-        } else {
-            ciOptions.printHelp();
-        }
-    }
+	public static void main(String[] args) {
+		CiOptions ciOptions = new CiOptions(args);
+		if (ciOptions.isValid()) {
+			new Ci(ciOptions).start();
+		} else {
+			ciOptions.printHelp();
+		}
+	}
 
-    public Ci(CiOptions ciOptions) {
-        this(ciOptions.type, ciOptions.root);
-    }
+	public Ci(CiOptions ciOptions) {
+		this(ciOptions.type, ciOptions.root);
+	}
 
-    private Ci(Ci oldCi) {
-        this(oldCi.type, oldCi.rootPath.toString());
-    }
+	private Ci(Ci oldCi) {
+		this(oldCi.type, oldCi.rootPath.toString());
+	}
 
-    public Ci(Type type, String root) {
-        LOGGER.debug("type: {}", type);
-        LOGGER.debug("root: {}", root);
-        this.type = type;
-        this.rootPath = Paths.get(root);
-        if (!exists(rootPath)) {
-            try {
-                Files.createDirectories(rootPath);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
-        if (!isDirectory(rootPath)) {
-            throw new IllegalArgumentException("CI root <" + root + "> is not a directory");
-        }
-        this.repositoriesPath = rootPath.resolve("repositories");
-        this.commandsPath = rootPath.resolve("commands");
-        this.workspacesPath = rootPath.resolve("workspaces");
-    }
+	public Ci(Type type, String root) {
+		LOGGER.debug("type: {}", type);
+		LOGGER.debug("root: {}", root);
+		this.type = type;
+		this.rootPath = Paths.get(root);
+		if (!exists(rootPath)) {
+			try {
+				Files.createDirectories(rootPath);
+			} catch (IOException e) {
+				throw new UncheckedIOException(e);
+			}
+		}
+		if (!isDirectory(rootPath)) {
+			throw new IllegalArgumentException("CI root <" + root + "> is not a directory");
+		}
+		this.repositoriesPath = rootPath.resolve("repositories");
+		this.commandsPath = rootPath.resolve("commands");
+		this.workspacesPath = rootPath.resolve("workspaces");
+	}
 
-    public String nodeId() {
-        return nodeId;
-    }
+	public String nodeId() {
+		return nodeId;
+	}
 
-    public Repositories repositories() {
-        return data.repositories();
-    }
+	public Repositories repositories() {
+		return data.repositories();
+	}
 
-    public Tasks tasks() {
-        return data.tasks();
-    }
+	public Tasks tasks() {
+		return data.tasks();
+	}
 
-    public Jobs jobs() {
-        return data.jobs();
-    }
+	public Jobs jobs() {
+		return data.jobs();
+	}
 
-    public JobQueue jobQueue() {
-        return data.jobQueue();
-    }
+	public JobQueue jobQueue() {
+		return data.jobQueue();
+	}
 
-    public void start() {
-        if (isRunning()) {
-            LOGGER.error("CI server is already running");
-            return;
-        }
-        LOGGER.debug("CI server starting...");
-        createDirectoriesIfNotExists();
-        initData();
-        startCommandProcessor();
-        startJobProcessor();
-        startRepositoryProcessor();
-        startEventProcessor();
-        do {
-            sleep();
-        } while (processorsIsNotStarted());
-        LOGGER.debug("CI server started");
-    }
+	public void start() {
+		if (isRunning()) {
+			LOGGER.error("CI server is already running");
+			return;
+		}
+		LOGGER.debug("CI server starting...");
+		createDirectoriesIfNotExists();
+		initData();
+		startCommandProcessor();
+		startJobSubscriber();
+		startRepositoryProcessor();
+		startEventProcessor();
+		do {
+			sleep();
+		} while (processorsIsNotStarted());
+		LOGGER.debug("CI server started");
+	}
 
-    private boolean isRunning() {
-        return nodeId != null;
-    }
+	private boolean isRunning() {
+		return nodeId != null;
+	}
 
-    private void createDirectoriesIfNotExists() {
-        createDirectoryIfNotExists(repositoriesPath);
-        createDirectoryIfNotExists(commandsPath);
-        createDirectoryIfNotExists(workspacesPath);
-    }
+	private void createDirectoriesIfNotExists() {
+		createDirectoryIfNotExists(repositoriesPath);
+		createDirectoryIfNotExists(commandsPath);
+		createDirectoryIfNotExists(workspacesPath);
+	}
 
-    public void createDirectoryIfNotExists(Path directoryPath) {
-        if (!exists(directoryPath)) {
-            try {
-                Files.createDirectory(directoryPath);
-                LOGGER.debug("Directory created: {}", directoryPath);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
-        if (!isDirectory(directoryPath)) {
-            throw new IllegalArgumentException(directoryPath + " is not a directory");
-        }
-    }
+	public void createDirectoryIfNotExists(Path directoryPath) {
+		if (!exists(directoryPath)) {
+			try {
+				Files.createDirectory(directoryPath);
+				LOGGER.debug("Directory created: {}", directoryPath);
+			} catch (IOException e) {
+				throw new UncheckedIOException(e);
+			}
+		}
+		if (!isDirectory(directoryPath)) {
+			throw new IllegalArgumentException(directoryPath + " is not a directory");
+		}
+	}
 
-    private void initData() {
-        if (type == HAZELCAST) {
-            Config config = new Config();
-            config.setProperty("hazelcast.logging.type", "slf4j");
-            HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(config);
-            this.data = new HazelcastData(hazelcastInstance);
-            LOGGER.debug("Hazelcast data store initialized");
-        } else {
-            this.data = new SimpleData();
-            LOGGER.debug("Simple data store initialized");
-        }
-        this.nodeId = data.nodeId();
-        LOGGER.debug("Node id: {}", nodeId);
-    }
+	private void initData() {
+		if (type == HAZELCAST) {
+			Config config = new Config();
+			config.setProperty("hazelcast.logging.type", "slf4j");
+			HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+			this.data = new HazelcastData(hazelcastInstance);
+			LOGGER.debug("Hazelcast data store initialized");
+		} else {
+			this.data = new SimpleData();
+			LOGGER.debug("Simple data store initialized");
+		}
+		this.nodeId = data.nodeId();
+		LOGGER.debug("Node id: {}", nodeId);
+	}
 
-    private void startCommandProcessor() {
-        this.commandProcessor = new CommandProcessor(this);
-        startNewThread(commandProcessor);
-    }
+	private void startCommandProcessor() {
+		this.commandProcessor = new CommandProcessor(this);
+		startNewThread(commandProcessor);
+	}
 
-    private void startJobProcessor() {
-        this.jobProcessor = new JobProcessor(this);
-        startNewThread(jobProcessor);
-    }
+	private void startJobSubscriber() {
+		this.jobSubscriber = new JobSubscriber(this);
+		jobSubscriber.start();
+	}
 
-    private void startRepositoryProcessor() {
-        this.repositoryProcessor = new RepositoryProcessor(this);
-        startNewThread(repositoryProcessor);
-    }
+	private void startRepositoryProcessor() {
+		this.repositoryProcessor = new RepositoryProcessor(this);
+		startNewThread(repositoryProcessor);
+	}
 
-    private void startEventProcessor() {
-        this.eventProcessor = new EventProcessor(this);
-        startNewThread(eventProcessor);
-    }
+	private void startEventProcessor() {
+		this.eventProcessor = new EventProcessor(this);
+		startNewThread(eventProcessor);
+	}
 
-    private boolean processorsIsNotStarted() {
-        return !commandProcessor.isStarted()
-                && !jobProcessor.isStarted()
-                && !repositoryProcessor.isStarted()
-                && !eventProcessor.isStarted();
-    }
+	private boolean processorsIsNotStarted() {
+		return !commandProcessor.isStarted()
+				&& !repositoryProcessor.isStarted()
+				&& !eventProcessor.isStarted();
+	}
 
-    private void startNewThread(Runnable target) {
-        String name = uncapitalize(target.getClass().getSimpleName());
-        startNewThread(target, name);
-    }
+	private void startNewThread(Runnable target) {
+		String name = uncapitalize(target.getClass().getSimpleName());
+		startNewThread(target, name);
+	}
 
-    private void startNewThread(Runnable target, String name) {
-        new Thread(target, name).start();
-    }
+	private void startNewThread(Runnable target, String name) {
+		new Thread(target, name).start();
+	}
 
-    public void shutdown() {
-        shutdown(false);
-    }
+	public void shutdown() {
+		shutdown(false);
+	}
 
-    public void restart() {
-        shutdown(true);
-    }
+	public void restart() {
+		shutdown(true);
+	}
 
-    private void shutdown(boolean restart) {
-        startNewThread(() -> {
-            LOGGER.debug("CI server shutting down...");
-            try {
-                stopProcessors();
-                do {
-                    sleep();
-                } while (processorsIsNotStopped());
-            } finally {
-                try {
-                    this.data.shutdown();
-                } finally {
-                    this.nodeId = null;
-                }
-            }
-            LOGGER.debug("CI server shutdown completed!");
-            if (restart) {
-                new Ci(this).start();
-            }
-        }, restart ? "restart" : "shutdown");
-    }
+	private void shutdown(boolean restart) {
+		startNewThread(() -> {
+			LOGGER.debug("CI server shutting down...");
+			try {
+				stopSubscribers();
+				stopProcessors();
+				do {
+					sleep();
+				} while (isSubscribersActive() || processorsIsNotStopped());
+			} finally {
+				try {
+					this.data.shutdown();
+				} finally {
+					this.nodeId = null;
+				}
+			}
+			LOGGER.debug("CI server shutdown completed!");
+			if (restart) {
+				new Ci(this).start();
+			}
+		}, restart ? "restart" : "shutdown");
+	}
 
-    private void stopProcessors() {
-        commandProcessor.stop();
-        jobProcessor.stop();
-        repositoryProcessor.stop();
-        eventProcessor.stop();
-    }
+	private void stopSubscribers() {
+		jobSubscriber.stop();
+	}
 
-    private void sleep() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            LOGGER.warn("Sleep interrupted", e);
-        }
-    }
+	private void stopProcessors() {
+		commandProcessor.stop();
+		repositoryProcessor.stop();
+		eventProcessor.stop();
+	}
 
-    private boolean processorsIsNotStopped() {
-        return !commandProcessor.isStopped()
-                || !jobProcessor.isStopped()
-                || !repositoryProcessor.isStopped()
-                || !eventProcessor.isStopped();
-    }
+	private void sleep() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			LOGGER.warn("Sleep interrupted", e);
+		}
+	}
 
-    public int nextJobNumberFor(TaskId taskId) {
-        return jobs()
-                .stream()
-                .filter(job -> job.taskId.equals(taskId))
-                .mapToInt(job -> job.jobNumber)
-                .max().orElse(0) + 1;
+	private boolean isSubscribersActive() {
+		return jobSubscriber.isActive();
+	}
 
-    }
+	private boolean processorsIsNotStopped() {
+		return !commandProcessor.isStopped()
+				|| !repositoryProcessor.isStopped()
+				|| !eventProcessor.isStopped();
+	}
+
+	public int nextJobNumberFor(TaskId taskId) {
+		return jobs()
+				.stream()
+				.filter(job -> job.taskId.equals(taskId))
+				.mapToInt(job -> job.jobNumber)
+				.max().orElse(0) + 1;
+
+	}
 }
--- src/main/java/no/lundesgaard/ci/model/job/JobQueue.java
+++ src/main/java/no/lundesgaard/ci/model/job/JobQueue.java
@@ -1,5 +1,8 @@
 package no.lundesgaard.ci.model.job;
 
+import rx.Observable;
+import rx.Subscriber;
+
 import java.util.NoSuchElementException;
 import java.util.Queue;
 
@@ -19,6 +22,31 @@ public class JobQueue {
 			return this.queue.remove();
 		} catch (NoSuchElementException e) {
 			return null;
+		}
+	}
+
+	public void subscribe(Subscriber<JobId> subscriber) {
+		jobIdObservable().subscribe(subscriber);
+	}
+
+	private Observable<JobId> jobIdObservable() {
+		return Observable.create(subscriber -> {
+			while (!subscriber.isUnsubscribed()) {
+				JobId nextJobId = next();
+				if (nextJobId != null) {
+					subscriber.onNext(nextJobId);
+				} else {
+					sleep();
+				}
+			}
+		});
+	}
+
+	private void sleep() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// ignore exception
 		}
 	}
 }
